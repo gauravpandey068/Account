72621e489d425226503c902c7f7776334a62c592
Gaurav Pandey <pandeygaurav068@gmail.com>
2020-09-22 15:21:37 +0545

    Account system

---
+++ account/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, EditUserForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, "UserName or Password not Match")
+                return render(request, 'login.html')
+        context = {
+
+        }
+        return render(request, 'login.html', context)
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Account was Created!')
+                return redirect('home')
+
+        context = {
+            'form': form
+
+        }
+        return render(request, 'signup.html', context)
+
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+@login_required(login_url='login')
+def profile(request):
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated!')
+            return redirect('home')
+
+    users = User.objects.get(first_name=request.user.first_name, last_name=request.user.last_name,
+                             email=request.user.email)
+    context = {
+        'users': users
+
+    }
+    return render(request, 'profile.html', context)
